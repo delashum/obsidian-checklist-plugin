b2e05a377d6d2ef41631aa0dbd709f9ee173632f
delashum <delashum@gmail.com>
2021-08-12 20:01:46 -0600

    remove some logs

--- src/plugins/plugin-helper.ts
+++ src/plugins/plugin-helper.ts
@@ -14,10 +14,6 @@ const utils = {
 
 let counter = 0
 
-/**
- * Constructor function
- */
-
 export const regexPlugin = (
   regexp: RegExp,
   replacer: (match: string[], utils: { escape: (html: string) => string }) => string
@@ -27,15 +23,12 @@ export const regexPlugin = (
   const id = "regexp-" + counter++
 
   return (md: MD) => {
-    console.log(md)
     md.inline.ruler.push(id, (state, silent) => {
       var match = _regexp.exec(state.src.slice(state.pos))
       if (!match) return false
 
-      // valid match found, now we need to advance cursor
       state.pos += match[0].length
 
-      // don't insert any tokens in silent mode
       if (silent) return true
 
       var token = state.push(id, "", 0)
