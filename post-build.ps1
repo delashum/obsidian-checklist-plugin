2385ad164a66d9d6e77140cf5f17e029a34cabbc
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <c.negrissa.the.angel@gmail.com>
2022-11-14 14:03:14 -0500

    remove post-build and editorconfig

--- post-build.ps1
+++
@@ -1,3 +0,0 @@
-$vault="$env:USERPROFILE\projs\obsidian-dev\test-vault" # replace with w/e
-Copy-Item .\main.js "$vault\.obsidian\plugins\obsidian-checklist-plugin"
-Copy-Item .\styles.css "$vault\.obsidian\plugins\obsidian-checklist-plugin"
